7e8b9e7a860d52ba0a1d31a625eb757cf3bd96ac
Shivam Verma <shivam.verma@atherenergy.com>
2022-10-16 13:20:38 +0530

    Showing mesg before join the room

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,18 @@
 import Link from "next/link";
-import { Col, Row } from "antd";
+import { Col, Row, Typography } from "antd";
 import { useRouter } from "next/router";
 import MetaTag from "./components/MetaTag";
 import { useState } from "react";
+import { Backdrop, CircularProgress } from "@mui/material";
 
 export default function Home() {
   const router = useRouter();
   const [isDisabled, setIsDisabled] = useState(false);
+  const [backdropState, setBackdropState] = useState(false);
 
   const joinRoom = () => {
     setIsDisabled(true);
+    setBackdropState(true);
     let roomId = Math.random().toString(36).slice(2);
     router.push(`tic-tac-toe/${roomId}`);
   };
@@ -17,6 +20,42 @@ export default function Home() {
   return (
     <div className="HomeContent">
       <MetaTag />
+
+      <Backdrop
+        open={backdropState}
+        sx={{
+          zIndex: (theme) => theme.zIndex.drawer + 1,
+        }}
+      >
+        <Row>
+          <Col md={24} xs={24}>
+            <Typography
+              variant="h4"
+              style={{
+                textAlign: "center",
+                top: 60,
+                position: "fixed",
+                left: 10,
+                right: 10,
+                fontFamily: "Hanalei Fill",
+                fontSize: "1.6em",
+                color: "whitesmoke",
+              }}
+            >
+              Hold tight, we are making room for you!
+            </Typography>
+          </Col>
+          <br />
+          <Col
+            md={24}
+            xs={24}
+            style={{ marginTop: "20px", textAlign: "center" }}
+          >
+            <CircularProgress style={{ color: "#ff5db1 !important" }} />
+          </Col>
+        </Row>
+      </Backdrop>
+
       <Row className="HomeImage">
         <Col
           xs={24}
